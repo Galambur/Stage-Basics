e23be97683b01a4aeb81cc4b4bc8eac745b15a52
Galambur <59735586+Galambur@users.noreply.github.com>
2020-07-09 10:34:14 +0200

    version 2.2
    version clean (normalement finale)

--- basicActions.py
+++ basicActions.py
@@ -42,19 +42,20 @@ class CheckBox(QWidget):
         self.group.addButton(self.change)
         self.group.addButton(self.delete)
         self.group.addButton(self.add)
-        self.group.addButton(self.copy)        
+        self.group.addButton(self.copy) 
+        self.setWindowTitle("Actions basiques")        
 
         
     def initGui(self):
       icon = os.path.join(os.path.join(cmd_folder, 'logo.png'))
-      self.action = QAction(QIcon(icon), 'Do basic actions on the attribute table', self.iface.mainWindow()) #written when hovered
+      self.action = QAction(QIcon(icon), 'Actions basiques QGis', self.iface.mainWindow()) #written when hovered
       self.action.triggered.connect(self.run) #runs run() when clicked
-      self.iface.addPluginToMenu('&Basic actions', self.action)
+      self.iface.addPluginToMenu('&Actions basiques', self.action)
       self.iface.addToolBarIcon(self.action)
 
     def unload(self):
       self.iface.removeToolBarIcon(self.action)       
-      self.iface.removePluginMenu('&Basic actions', self.action)  
+      self.iface.removePluginMenu('&Actions basiques', self.action)  
       del self.action  
 
     def run(self):
@@ -64,17 +65,17 @@ class CheckBox(QWidget):
     # First radio buttons
     def change_active(self, on):
         if on:
-            #print("changing")
+            ##print("changing")
             self.close() #ferme la checkbox
             layer = iface.activeLayer()
             prov = layer.dataProvider()
             items = ([field.name() for field in prov.fields()])
-            item, ok = QInputDialog.getItem(self, "select a name to change", 
-         "list of fields", items, 0, False) # get field to delete with selector
+            item, ok = QInputDialog.getItem(self, "Sélectionnez un nom à changer", 
+         "Liste des champs", items, 0, False) # get field to delete with selector
             if ok and item:
                 change_name(item) # send name of the field to function
             else : 
-                print("Pas de champ entré")
+                #print("Pas de champ entré")
                 iface.messageBar().pushWarning("Erreur", "Vous n'avez pas entré de champ à changer")
             self.group.setExclusive(False)   #3 lines to uncheck the button (for next use) 
             self.change.setChecked(False) 
@@ -84,17 +85,17 @@ class CheckBox(QWidget):
     # Second radio buttons    
     def delete_active(self, on):
         if on:
-            #print("delete")
+            ##print("delete")
             self.close()
             layer = iface.activeLayer()
             prov = layer.dataProvider()
             items = ([field.name() for field in prov.fields()])
-            item, ok = QInputDialog.getItem(self, "select a field to delete", 
-         "list of fields", items, 0, False) # get field to delete with selector
+            item, ok = QInputDialog.getItem(self, "Sélectionnez un champ à supprimer", 
+         "Liste des champs", items, 0, False) # get field to delete with selector
             if ok and item:
                 delete_column(item) # send name of the field to function
             else : 
-                print("Pas de champ entré")
+                #print("Pas de champ entré")
                 iface.messageBar().pushWarning("Erreur", "Vous n'avez pas entré de champ à supprimer")
             self.group.setExclusive(False)    
             self.delete.setChecked(False) 
@@ -104,7 +105,7 @@ class CheckBox(QWidget):
     #Third radio buttons
     def add_active(self, on):
         if on :
-            #print("adding")
+            ##print("adding")
             self.close()
             add_column()
             self.group.setExclusive(False)    
@@ -114,27 +115,27 @@ class CheckBox(QWidget):
         
     def copy_active(self, on):
         if on :
-            #print("copying")
+            ##print("copying")
             self.close()
             layer = iface.activeLayer()
             prov = layer.dataProvider()
             items = ([field.name() for field in prov.fields()])
-            field_to_copy, ok = QInputDialog.getItem(self, "select a field to delete", 
+            field_to_copy, ok = QInputDialog.getItem(self, "Sélectionnez le champ à copier", 
          "Copier les valeurs de...", items, 0, False) # get field to delete with selector
             if ok and field_to_copy:
                 items = ([field.name() for field in prov.fields()])
-                field_to_paste, ok = QInputDialog.getItem(self, "select a field to delete", 
+                field_to_paste, ok = QInputDialog.getItem(self, "Sélectionner le champ pour coller", 
              "Coller les valeurs dans...", items, 0, False) 
-                print(field_to_copy)
+                #print(field_to_copy)
                 if ok and field_to_paste:
-                    print(field_to_paste)
-                    print(field_to_copy, field_to_paste)
+                    #print(field_to_paste)
+                    #print(field_to_copy, field_to_paste)
                     copy_column(field_to_copy, field_to_paste)  
                 else : 
-                    print("Pas de champ entré")
+                    #print("Pas de champ entré")
                     iface.messageBar().pushWarning("Erreur", "Vous n'avez pas entré de champ à coller")
             else : 
-                print("Pas de champ entré")
+                #print("Pas de champ entré")
                 iface.messageBar().pushWarning("Erreur", "Vous n'avez pas entré de champ à copier")
             self.group.setExclusive(False)    
             self.copy.setChecked(False) 
@@ -152,7 +153,7 @@ buttons = CheckBox()
 def getTextInput(title, message):
     answer = QInputDialog.getText(None, title, message)
     if answer[1]:
-        #print(answer[0])
+        ##print(answer[0])
         return answer[0]
     else:
         return None
@@ -177,11 +178,11 @@ def change_name(old_name):
                 layer.renameAttribute(id, new_name)
                 layer.commitChanges() #commiting changes
                 iface.vectorLayerTools().stopEditing(layer) #exit edition mode
-                print("La colonne {} s'appelle maintenant {}".format(old_name, new_name))
+                #print("La colonne {} s'appelle maintenant {}".format(old_name, new_name))
                 iface.messageBar().pushSuccess("Succes", "Le nom de la colonne a bien été modifiée")
                 check=1
             else:                 
-                print("La colonne {} existe déjà dans {}".format(new_name, layer.name()))
+                #print("La colonne {} existe déjà dans {}".format(new_name, layer.name()))
                 iface.messageBar().pushWarning("Erreur", "Ce nom de colonne existe déjà")
                 check=1
             
@@ -198,21 +199,18 @@ def add_column():
     field_to_add = layer.fields().indexFromName(field_name)
     prov = layer.dataProvider()
     field_names = [field.name() for field in prov.fields()]
-    
-    for field in prov.fields():
-        print(field.name() )
       
 
     if field_to_add == -1 :
         adding_test = layer.dataProvider().addAttributes([ QgsField(field_name, QVariant.String) ])
         if adding_test == False:
-            print("Un problème est survenu")
+            #print("Un problème est survenu")
             iface.messageBar().pushWarning("Erreur", "Un problème est survenu")
         else:
-            print("La colonne '", field_name, "' a été ajoutée")
+            #print("La colonne '", field_name, "' a été ajoutée")
             iface.messageBar().pushSuccess("Succès", "La colonne a été ajoutée")
     else : 
-        print("La colonne numero ", field_to_add, " (", field_name, ") existe déjà")
+        #print("La colonne numero ", field_to_add, " (", field_name, ") existe déjà")
         iface.messageBar().pushWarning("Erreur", "Cette colonne existe déjà")
     layer.updateFields()
 
@@ -224,14 +222,15 @@ def delete_column(field_name):
     field_to_delete = layer.fields().indexFromName(field_name)
 
     if field_to_delete == -1 :
-        print("La colonne '", field_name, "' n'a pas pu être trouvée")
+        iface.messageBar().pushWarning("Erreur", "La colonne n'a pas pu être trouvée")
+        #print("La colonne '", field_name, "' n'a pas pu être trouvée")
     else : 
         delete_test = layer.dataProvider().deleteAttributes([field_to_delete])  
         if delete_test == True : 
-            print("La colonne numero ", field_to_delete, " (", field_name, ") a été supprimée")
+            #print("La colonne numero ", field_to_delete, " (", field_name, ") a été supprimée")
             iface.messageBar().pushSuccess("Succès", "La colonne a été supprimée")       
         else : 
-            print("Un problème est survenu")
+            #print("Un problème est survenu")
             iface.messageBar().pushWarning("Erreur", "Un problème est survenu")
     
     layer.updateFields()
@@ -241,14 +240,11 @@ def delete_column(field_name):
 #Function to copy the values of a column and add them in another column
 def copy_column(field_to_copy, field_to_paste):
     layer = iface.activeLayer()
-    
-    #field_to_copy = getTextInput("Copier les valeurs d'une colonne", 'Veuillez entrer le nom de la colonne dont vous voulez copier les valeurs')
-    #field_to_paste = getTextInput("Copier les valeurs d'une colonne", 'Veuillez entrer le nom de la colonne dans laquelle copier les valeurs')
-    
+        
     id_field_to_copy = layer.fields().indexFromName(field_to_copy) # retourne -1 si l'id n'existe pas
     id_field_to_paste = layer.fields().indexFromName(field_to_paste)
     
-    print("copier : ", id_field_to_copy, " coller : ", id_field_to_paste)
+    #print("copier : ", id_field_to_copy, " coller : ", id_field_to_paste)
     
     if id_field_to_copy != -1 and field_to_paste != -1:
         layer.startEditing()
@@ -258,4 +254,4 @@ def copy_column(field_to_copy, field_to_paste):
         layer.commitChanges()
         iface.vectorLayerTools().stopEditing(layer)
         iface.messageBar().pushSuccess("Succès", "Les valeurs ont été copiées")
-        print("Les valeurs de la colonne {} ont bien été copiées dans {}".format(field_to_copy, field_to_paste))
+        #print("Les valeurs de la colonne {} ont bien été copiées dans {}".format(field_to_copy, field_to_paste))
